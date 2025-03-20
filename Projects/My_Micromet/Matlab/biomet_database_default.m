7bd1c23f729899ffdf7707e1d74e13954ce651b5
Marcella Cross <mjc467@student.ubc.ca>
2025-03-20 14:46:24 -0700

    reorganizing2

---
+++ Projects/My_Micromet/Matlab/biomet_database_default.m
@@ -0,0 +1,3 @@
+function folderDatabase = biomet_database_default
+% This file is generated automatically by set_TAB_project.m('/Users/mcross/Desktop/FluxProjectPipeline/MicroMet/Projects/My_Micromet/')
+folderDatabase = '/Users/mcross/Desktop/FluxProjectPipeline/MicroMet/Projects/My_Micromet/Database';
